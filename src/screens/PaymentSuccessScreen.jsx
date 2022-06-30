664a26f0ef996f4f8ce794f538b55e5e4b1fd5d9
Legion <pawel.kapusta@student.uj.edu.pl>
2022-06-30 19:18:19 +0200

    fix

--- src/screens/PaymentSuccessScreen.jsx
+++ src/screens/PaymentSuccessScreen.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import { useNavigate, useParams, useSearchParams } from "react-router-dom";
 import { Typography } from "@mui/material";
 import Link from "@mui/material/Link";
@@ -11,7 +11,6 @@ const PaymentSuccessScreen = () => {
   const email = localStorage.getItem("orderEmail")
     ? localStorage.getItem("orderEmail")
     : localStorage.getItem("email");
-  let loading = false;
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -19,10 +18,7 @@ const PaymentSuccessScreen = () => {
       fetch("http://localhost:8080/api/v1/order/" + param.orderID + "/" + email + "/" + paymentId, {
         method: "POST",
       })
-        .then(res => res.json())
-        .then(() => {
-          loading = true;
-        });
+        .then(res => res.json());
     }
   }, [email, param.orderID, paymentId, status]);
 
@@ -36,25 +32,29 @@ const PaymentSuccessScreen = () => {
 
   return (
     <div>
-      <div style={{ display: "flex", justifyContent: "center" }}>
-        <img src="https://m.media-amazon.com/images/I/41Gb3UOjT5L.jpg" alt="ThankYouImage" />
-      </div>
-      <Typography style={{ display: "flex", justifyContent: "center" }} variant="h6" color="grey">
-        If you want to see your orders click
-        <Link
-          style={{
-            marginLeft: 8,
-            cursor: "pointer",
-            fontSize: "1.2rem",
-            fontWeight: 600,
-          }}
-          variant="outlined"
-          onClick={handleRedirectToOrders}
-        >
-          {" "}
-          here
-        </Link>
-      </Typography>
+          <div style={{ display: "flex", justifyContent: "center" }}>
+            <img src="https://m.media-amazon.com/images/I/41Gb3UOjT5L.jpg" alt="ThankYouImage" />
+          </div>
+          <Typography
+            style={{ display: "flex", justifyContent: "center" }}
+            variant="h6"
+            color="grey"
+          >
+            If you want to see your orders click
+            <Link
+              style={{
+                marginLeft: 8,
+                cursor: "pointer",
+                fontSize: "1.2rem",
+                fontWeight: 600,
+              }}
+              variant="outlined"
+              onClick={handleRedirectToOrders}
+            >
+              {" "}
+              here
+            </Link>
+          </Typography>
     </div>
   );
 };
